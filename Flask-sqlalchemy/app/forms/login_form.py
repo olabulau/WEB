a0e000f91a9f137e93d609f4e4828e515ea2c83d
olabulau <117658045+olabulau@users.noreply.github.com>
2023-03-02 18:05:16 +0300

    Add files via upload

---
+++ Flask-sqlalchemy/app/forms/login_form.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, BooleanField, SubmitField, EmailField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    boolead = BooleanField('Remember me')
+    submit = SubmitField('Sing in')
---
+++ Flask-sqlalchemy/app/добавление работы.py
@@ -0,0 +1,16 @@
+@app.route('/addjob', methods=['GET', 'POST'])
+def addjob():
+    add_form = AddJobForm()
+    if add_form.validate_on_submit():
+        db_sess = db_session.create_session()
+        jobs = Jobs(
+            job=add_form.job.data,
+            team_leader=add_form.team_leader.data,
+            work_size=add_form.work_size.data,
+            collaborators=add_form.collaborators.data,
+            is_finished=add_form.is_finished.data
+        )
+        db_sess.add(jobs)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('addjob.html', title='Adding a job', form=add_form)
